0f4580f492d90b6501f16a54c81558a234b7f710
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-07-05 21:24:04 +0500

    added first blanks for database

---
+++ database/table_admins.py
@@ -0,0 +1,15 @@
+
+
+def add_admin(user_id):
+    # TODO: THIS
+    return 0
+
+
+def revoke_admin(user_id):
+    # TODO: THIS
+    return 0
+
+
+def is_admin(user_id) -> bool:
+    # TODO: THIS
+    return 0
