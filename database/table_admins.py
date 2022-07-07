3601540fecdbe882b0d2bcbf9ec1c0e5b2769456
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-07-07 22:39:52 +0500

    Template for parser_controller

--- database/table_admins.py
+++ database/table_admins.py
@@ -3,24 +3,24 @@ connection = sqlite3.connect('h.db')
 cursor = connection.cursor()
 
 
-def add_admin(user_id):
+def add_admin(user_id: str):
     cursor.execute("insert into admins(id_admin) values(?)", user_id)
     connection.commit()    
     return
 
 
-def revoke_admin(user_id):
+def revoke_admin(user_id: str):
     cursor.execute(f"delete from admins where id_admin = {user_id}")
     connection.commit() 
     return
 
 
-def is_admin(user_id) -> bool:  
+def is_admin(user_id: str) -> bool:
     admin_id = [x[0] for x in cursor.execute(f"select id_admin from admins where id_admin = {user_id}").fetchall()]
     return len(admin_id) > 0
 
 
 def create_table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS admins(id_admin NUMERIC(20,0))")
+    cursor.execute("CREATE TABLE IF NOT EXISTS admins(id_admin VARCHAR(36))")
     connection.commit() 
     return
