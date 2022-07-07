a73520593e8a97548a7b6c02ddbb9c9935e3de6e
Elizaveta923 <81793376+Elizaveta923@users.noreply.github.com>
2022-07-07 21:32:04 +0300

    Update entity_user.py

--- database/entity_user.py
+++ database/entity_user.py
@@ -4,4 +4,11 @@ class EntityUser:
     balance = int  # INTEGER
     login = str  # VARCHAR(36), UNIQUE
     password_hash = int  # INTEGER
-
+    
+     def __create_table__():
+        cursor.execute("PRAGMA foreign_keys=on")
+        cursor.execute("CREATE TABLE IF NOT EXISTS EntityRequest(location_lon NUMERIC(10), "
+                       "location_lat NUMERIC(10), date_arrive DATETIME, date_depart DATETIME, "
+                       "date_request DATETIME, optional_filters TEXT,REFERENCES flight(id_number_trip),"
+                       "FOREIGN KEY (user_id) REFERENCES EntityUser(unique_id))")
+        connection.commit()
