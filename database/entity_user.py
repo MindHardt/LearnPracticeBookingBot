22a395779333985ccbe29a6b4004f2bd54fb6f6b
Elizaveta923 <81793376+Elizaveta923@users.noreply.github.com>
2022-07-07 21:43:40 +0300

    Update entity_user.py

--- database/entity_user.py
+++ database/entity_user.py
@@ -8,6 +8,6 @@ class EntityUser:
     def __create_table__():
     cursor.execute("PRAGMA foreign_keys=on")
     cursor.execute("CREATE TABLE IF NOT EXISTS EntityUser(unique_id PRIMARY KEY, "
-                       "name VARCHAR(36), balance INTEGER, login INTEGER UNIQUE, "
+                       "name VARCHAR(36), balance INTEGER, login VARCHAR(36) UNIQUE, "
                        "password_hash INTEGER")
     connection.commit()
